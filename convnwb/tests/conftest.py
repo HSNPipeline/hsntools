7ac37ef728bb174292d75072ed75e16b03f1a51e
Tom Donoghue <tdonoghue.research@gmail.com>
2021-10-22 22:07:48 -0400

    add test infrastructure

---
+++ convnwb/tests/conftest.py
@@ -0,0 +1,23 @@
+"""Pytest configuration file for testing convnwb."""
+
+import os
+import shutil
+import pytest
+
+from spiketools.tests.tsettings import TEST_FILE_PATH
+
+###################################################################################################
+###################################################################################################
+
+## TEST SETUP
+
+@pytest.fixture(scope='session', autouse=True)
+def check_dir():
+    """Once, prior to session, this will clear and re-initialize the test file directories."""
+
+    # If the directories already exist, clear them
+    if os.path.exists(TEST_FILE_PATH):
+        shutil.rmtree(TEST_FILE_PATH)
+
+    # Remake (empty) directories
+    os.mkdir(TEST_FILE_PATH)
---
+++ convnwb/tests/tsettings.py
@@ -0,0 +1,10 @@
+"""Settings for tests."""
+
+import os
+import pkg_resources as pkg
+
+###################################################################################################
+###################################################################################################
+
+# Set paths for test files
+TEST_FILE_PATH = pkg.resource_filename(__name__, 'test_files')
