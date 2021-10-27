7bdf690d0bb7d4ad6d74cfb53cef7221a42dbd8a
Tom Donoghue <tdonoghue.research@gmail.com>
2021-10-26 23:55:35 -0400

    add timestamp tests

--- convnwb/tests/test_timestamps.py
+++ convnwb/tests/test_timestamps.py
@@ -1,9 +1,17 @@
 """Tests for convnwb.timestamps"""
 
+import numpy as np
+
 from convnwb.timestamps import *
 
 ###################################################################################################
 ###################################################################################################
 
 def test_align_times():
-    pass
+
+    t1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    t2 = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    intercept, coef, score = align_times(t1, t2)
+
+    model, score = align_times(t1, t2, return_model=True)
