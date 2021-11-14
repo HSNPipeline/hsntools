0ca33aaa3677c54f2f8cb74c5c7f761f51d6cdf7
Tom Donoghue <tdonoghue.research@gmail.com>
2021-11-14 18:02:43 -0500

    make test file path a Path object

--- convnwb/tests/tsettings.py
+++ convnwb/tests/tsettings.py
@@ -1,10 +1,11 @@
 """Settings for tests."""
 
 import os
+from pathlib import Path
 import pkg_resources as pkg
 
 ###################################################################################################
 ###################################################################################################
 
 # Set paths for test files
-TEST_FILE_PATH = pkg.resource_filename(__name__, 'test_files')
+TEST_FILE_PATH = Path(pkg.resource_filename(__name__, 'test_files'))
