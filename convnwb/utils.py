3d15a9aa7a1e110320428bafb6250f94a8ab50e5
Tom Donoghue <tdonoghue.research@gmail.com>
2022-04-07 15:59:37 -0400

    add time utils

--- convnwb/utils.py
+++ convnwb/utils.py
@@ -104,3 +104,34 @@ def list_str_to_bool(lst):
     """
 
     return [str_to_bool(el) for el in lst]
+
+
+def offset_time(times, offset):
+    """Apply an offset to timestamps.
+
+    Parameters
+    ----------
+    times : 1d array
+        Vector of timestamps to update.
+    offset : float
+        The time value to subtract from each logged time value.
+    """
+
+    return times - offset
+
+
+def change_time_units(times, value, operation='divide'):
+    """Change the units of timestamps.
+
+    Parameters
+    ----------
+    times : 1d array
+        Vector of timestamps to update.
+    value : float
+        Value to divide / multiply by.
+    operation : {'divide', 'multiply'}
+        Operation to apply.
+    """
+
+    func = {'divide' : np.divide, 'multiply' : np.multiply}[operation]
+    return func(times, value)
