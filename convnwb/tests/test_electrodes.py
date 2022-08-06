5a183e6805ab16e43061028e5f450ff821a6c14b
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-06 13:28:23 -0400

    add tests for n_bundles & iter

--- convnwb/tests/test_electrodes.py
+++ convnwb/tests/test_electrodes.py
@@ -10,7 +10,7 @@ def test_electrodes():
     electrodes = Electrodes()
     assert electrodes
 
-def test_electrodes_add_bundles():
+def test_electrodes_add_bundle():
 
     bundle_name = 'name'
     bundle_location = 'location'
@@ -20,3 +20,21 @@ def test_electrodes_add_bundles():
 
     assert bundle_name in electrodes.bundles
     assert bundle_location in electrodes.locations
+
+def test_electrodes_n_bundles():
+
+    electrodes = Electrodes()
+    electrodes.add_bundles(['n1', 'n2'], ['l1', 'l2'])
+    assert electrodes.n_bundles == 2
+
+def test_electrodes_iter():
+
+    names = ['n1', 'n2']
+    locations = ['l1', 'l2']
+    electrodes = Electrodes()
+    electrodes.add_bundles(names, locations)
+
+    for ind, (name, loc) in enumerate(electrodes):
+        assert name in names
+        assert loc in locations
+    assert ind == len(names) - 1
