76fe024fadf5110bae0f42f3521a2ec2235a1611
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-09 17:48:32 -0400

    update timezone handling for date utils

--- convnwb/utils.py
+++ convnwb/utils.py
@@ -8,28 +8,50 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
-def get_current_date():
+def get_current_date(tz=None):
     """Get the current datetime.
+
+    Parameters
+    ----------
+    tz : dateutil.tz.tz, optional
+        Timezone information. If not provided, defaults to the local time zone.
 
     Returns
     -------
-    datetime.datetime
+    date : datetime.datetime
         The current date information.
     """
 
-    return datetime.now(tzlocal())
+    if not tz:
+        tz = tzlocal()
+
+    date = datetime.now(tzlocal())
+
+    return date
 
 
 def convert_time_to_date(timestamp, tz=None):
     """Convert a time value to a datetime date.
 
+    Parameters
+    ----------
+    timestamp : float
+        Timestamp value to convert.
+    tz : dateutil.tz.tz, optional
+        Timezone information. If not provided, defaults to the local time zone.
+
     Returns
     -------
-    datetime.datetime
+    date : datetime.datetime
         Date corresponding to the given timestamp.
     """
 
-    return datetime.fromtimestamp(timestamp, tz=tz)
+    if not tz:
+        tz = tzlocal()
+
+    date = datetime.fromtimestamp(timestamp, tz=tz)
+
+    return date
 
 
 def is_empty(var):
