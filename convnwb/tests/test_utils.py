bd1a20cb0e154eeb488aabee2d3c39f77c6a0843
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-09 23:48:58 -0400

    add print_status util

--- convnwb/tests/test_utils.py
+++ convnwb/tests/test_utils.py
@@ -10,6 +10,11 @@ from convnwb.utils import *
 ###################################################################################################
 ###################################################################################################
 
+def test_print_status():
+
+    print_status(True, 'words, words, words', 1)
+    print_status(False, 'words, words, words', 2)
+
 def test_get_current_date():
 
     date = get_current_date()
--- convnwb/utils.py
+++ convnwb/utils.py
@@ -8,6 +8,23 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
+def print_status(verbose, message, level=1):
+    """Print a status update.
+
+    Parameters
+    ----------
+    verbose : bool
+        Whether to print.
+    message : str
+        Text to print out.
+    level : int
+        Indentation level.
+    """
+
+    if verbose:
+        print('\t' * level + message)
+
+
 def get_current_date(tz=None):
     """Get the current datetime.
 
