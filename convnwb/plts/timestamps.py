51dc776c10c13d323a8d61d5802ac5d99e22be72
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-23 11:31:52 -0400

    add plot_peaks

--- convnwb/plts/timestamps.py
+++ convnwb/plts/timestamps.py
@@ -35,3 +35,28 @@ def plot_alignment(sync1, sync2, n_pulses=None, ax=None, **plt_kwargs):
 
     if n_pulses:
         ax.set_xlim(sync1[0], sync1[n_pulses])
+
+
+@savefig
+@check_dependency(plt, 'matplotlib')
+def plot_peaks(data, peak_inds, peak_heights, ax=None, **plt_kwargs):
+    """Plot detected peaks on a time series.
+
+    Parameters
+    ----------
+    data : 1d array
+        Time series data.
+    peak_inds : 1d array
+        Indices of the detected peaks.
+    peak_heights : 1d array
+        Heights of the detected peaks.
+    ax : Axes, optional
+        Axis object upon which to plot.
+    plt_kwargs
+        Additional arguments to pass into the plot function.
+    """
+
+    ax = check_ax(ax, figsize=plt_kwargs.pop('figsize', (12, 4)))
+
+    ax.plot(data)
+    ax.plot(peak_inds, peak_heights, '.')
--- convnwb/tests/plts/test_timestamps.py
+++ convnwb/tests/plts/test_timestamps.py
@@ -1,5 +1,7 @@
 """Tests for convnwb.plts.timestamps"""
 
+import numpy as np
+
 from convnwb.tests.tutils import plot_test
 from convnwb.tests.tsettings import TEST_PLOTS_PATH
 
@@ -15,3 +17,13 @@ def test_plot_alignment():
     arr2 = [1, 2, 3, 4, 5]
 
     plot_alignment(arr1, arr2)
+
+@plot_test
+def test_plot_peaks():
+
+    data = np.array([0, 0, 0, 1, 0, 0, 0, 1, 0, 0])
+
+    peak_inds = np.array([3, 7])
+    peak_heights = np.array([1, 1])
+
+    plot_peaks(data, peak_inds, peak_heights)
