37b37887b3294a7ec9952ec5599a0323d0d016e9
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-23 11:18:48 -0400

    use plot utils

--- convnwb/plts/timestamps.py
+++ convnwb/plts/timestamps.py
@@ -1,14 +1,16 @@
 """Visualizations for checking timestamps."""
 
 from convnwb.modutils.dependencies import safe_import, check_dependency
+from convnwb.plts.utils import check_ax, savefig
 
 plt = safe_import('.pyplot', 'matplotlib')
 
 ###################################################################################################
 ###################################################################################################
 
+@savefig
 @check_dependency(plt, 'matplotlib')
-def plot_alignment(sync1, sync2, n_pulses=None):
+def plot_alignment(sync1, sync2, n_pulses=None, ax=None, **plt_kwargs):
     """Plot the alignment between synchronization pulses.
 
     Parameters
@@ -17,9 +19,14 @@ def plot_alignment(sync1, sync2, n_pulses=None):
         Sync pulse times for each sync pulse stream.
     n_pulses : int, optional
         Restrict the visualization to a restricted number of pulses.
+    ax : Axes, optional
+        Axis object upon which to plot.
+    plt_kwargs
+        Additional arguments to pass into the plot function.
     """
 
-    _, ax = plt.subplots(figsize=(20, 4))
+    ax = check_ax(ax, figsize=plt_kwargs.pop('figsize', (20, 4)))
+
     ax.eventplot([sync1, sync2], linelengths=[0.9, 0.9], colors=['g', 'b'])
     ax.set_xlabel('Time (ms)')
     ax.set_yticks([0, 1])
