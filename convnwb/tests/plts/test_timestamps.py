0f9ab00849263f354c1d1734d98e3ce0f507b9bf
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-23 11:21:01 -0400

    update plot testing

--- convnwb/tests/plts/test_timestamps.py
+++ convnwb/tests/plts/test_timestamps.py
@@ -1,17 +1,17 @@
 """Tests for convnwb.plts.timestamps"""
 
+from convnwb.tests.tutils import plot_test
+from convnwb.tests.tsettings import TEST_PLOTS_PATH
+
 from convnwb.plts.timestamps import *
 
 ###################################################################################################
 ###################################################################################################
 
+@plot_test
 def test_plot_alignment():
 
     arr1 = [1, 2, 3, 4, 5]
     arr2 = [1, 2, 3, 4, 5]
 
     plot_alignment(arr1, arr2)
-
-    # Check that the plot was created
-    ax = plt.gca()
-    assert ax.has_data()
