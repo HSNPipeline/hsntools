e81a5a4e27b4a4d95f4e31aa2a55b1fb370c1d48
Tom Donoghue <tdonoghue.research@gmail.com>
2022-08-23 11:33:43 -0400

    add test plot outputs

--- convnwb/tests/plts/test_timestamps.py
+++ convnwb/tests/plts/test_timestamps.py
@@ -16,7 +16,8 @@ def test_plot_alignment():
     arr1 = [1, 2, 3, 4, 5]
     arr2 = [1, 2, 3, 4, 5]
 
-    plot_alignment(arr1, arr2)
+    plot_alignment(arr1, arr2,
+                   file_path=TEST_PLOTS_PATH, file_name='tplot_alignment.png')
 
 @plot_test
 def test_plot_peaks():
@@ -26,4 +27,5 @@ def test_plot_peaks():
     peak_inds = np.array([3, 7])
     peak_heights = np.array([1, 1])
 
-    plot_peaks(data, peak_inds, peak_heights)
+    plot_peaks(data, peak_inds, peak_heights,
+               file_path=TEST_PLOTS_PATH, file_name='tplot_peaks.png')
