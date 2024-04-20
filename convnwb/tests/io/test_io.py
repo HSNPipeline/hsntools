e6061fa753509266a3e2503f7868f1ff13de8e6b
Tom Donoghue <tdonoghue.research@gmail.com>
2024-04-20 12:57:05 -0400

    clean up IO tests

--- convnwb/tests/io/test_io.py
+++ convnwb/tests/io/test_io.py
@@ -17,7 +17,7 @@ def test_save_nwbfile(tnwbfile):
 
     test_fname = 'test_nwbfile'
     save_nwbfile(tnwbfile, test_fname, TEST_FILE_PATH)
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, test_fname + '.nwb'))
+    assert os.path.exists(TEST_FILE_PATH / (test_fname + '.nwb'))
 
 def test_load_nwbfile():
 
@@ -31,13 +31,13 @@ def test_save_config():
     f_name1 = 'test_config1'
     save_config(cdict1, f_name1, TEST_FILE_PATH)
 
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, f_name1 + '.yaml'))
+    assert os.path.exists(TEST_FILE_PATH / (f_name1 + '.yaml'))
 
     cdict2 = {'d1' : 'words', 'd2' : None, 'd3' : ['list', 'of', 'terms']}
     f_name2 = 'test_config2'
     save_config(cdict2, f_name2, TEST_FILE_PATH)
 
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, f_name2 + '.yaml'))
+    assert os.path.exists(TEST_FILE_PATH / (f_name2 + '.yaml'))
 
 def test_load_config():
 
@@ -57,7 +57,7 @@ def test_save_task_object():
     f_name = 'task_obj'
     save_task_object(task, f_name, TEST_FILE_PATH)
 
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, f_name + '.task'))
+    assert os.path.exists(TEST_FILE_PATH / (f_name + '.task'))
 
 def test_load_task_object():
 
@@ -71,7 +71,7 @@ def test_save_txt():
     f_name = 'test_txt'
 
     save_txt(text, f_name, TEST_FILE_PATH)
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, f_name + '.txt'))
+    assert os.path.exists(TEST_FILE_PATH / (f_name + '.txt'))
 
 def test_load_txt():
 
@@ -85,7 +85,7 @@ def test_save_json():
     f_name = 'test_json'
 
     save_json(data, f_name, TEST_FILE_PATH)
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, f_name + '.json'))
+    assert os.path.exists(TEST_FILE_PATH / (f_name + '.json'))
 
 def test_load_json():
 
@@ -100,7 +100,7 @@ def test_save_jsonlines():
     f_name = 'test_jsonlines'
 
     save_jsonlines(data, f_name, TEST_FILE_PATH)
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, f_name + '.json'))
+    assert os.path.exists(TEST_FILE_PATH / (f_name + '.json'))
 
 def test_load_jsonlines():
 
@@ -141,19 +141,23 @@ def test_save_to_h5file():
         'data2' : np.array([1.5, 2.5, 3.5, 4.5]),
     }
 
-    test_fname = 'test_hdf52'
+    test_fname = 'test_hdf5_saved'
 
     save_to_h5file(tdata, test_fname, TEST_FILE_PATH)
-    assert os.path.exists(os.path.join(TEST_FILE_PATH, test_fname + '.h5'))
+    assert os.path.exists(TEST_FILE_PATH / (test_fname + '.h5'))
 
 def test_load_from_h5file():
 
-    f_name = 'test_hdf5'
+    # Note: this test loads data saved from `test_save_to_h5file`
+    f_name = 'test_hdf5_saved'
 
     # Test loading single field
-    dataset = load_from_h5file('data', f_name, TEST_FILE_PATH)
+    dataset = load_from_h5file('data1', f_name, TEST_FILE_PATH)
     assert dataset is not None
+    assert np.all(dataset['data1'])
 
     # Test loading multiple fields
-    datasets = load_from_h5file(['data', 'data2'], f_name, TEST_FILE_PATH)
+    datasets = load_from_h5file(['data1', 'data2'], f_name, TEST_FILE_PATH)
     assert datasets is not None
+    assert np.all(datasets['data1'])
+    assert np.all(datasets['data2'])
