de9d10d60c55943c8ef213f9f954f9a780373d7c
Tom Donoghue <tdonoghue.research@gmail.com>
2024-05-25 11:09:02 -0400

    fix check func again

--- convnwb/io/check.py
+++ convnwb/io/check.py
@@ -28,7 +28,7 @@ def check_blackrock_file_info(reader):
         n_segments = reader.segment_count(bi)
         for si in range(n_segments):
             seg_start = reader.segment_t_start(bi, si)
-            seg_end = reader.segment_t_stop(bi, si)
+            seg_stop = reader.segment_t_stop(bi, si)
             seg_size = reader.get_signal_size(bi, si)
             seg_length = convert_samples_to_time(seg_size, fs)
             print(str_fmt.format(si, seg_start, seg_stop, seg_size, seg_len))
