746ceead18a4b5b81c1ae221a5022b229bf04be4
Tom Donoghue <tdonoghue.research@gmail.com>
2025-09-02 22:02:28 +0100

    move date related functions to own file

--- hsntools/utils/convert.py
+++ hsntools/utils/convert.py
@@ -1,8 +1,5 @@
 """Utility functions for converting data types."""
 
-from datetime import datetime
-from dateutil.tz import tzlocal
-
 import numpy as np
 
 ###################################################################################################
@@ -83,27 +80,3 @@ def convert_to_array(data, dtype):
     array = np.array(data).astype(dtype)
 
     return array
-
-
-def convert_time_to_date(timestamp, tz=None):
-    """Convert a time value to a datetime date.
-
-    Parameters
-    ----------
-    timestamp : float
-        Timestamp value to convert.
-    tz : dateutil.tz.tz, optional
-        Timezone information. If not provided, defaults to the local time zone.
-
-    Returns
-    -------
-    date : datetime.datetime
-        Date corresponding to the given timestamp.
-    """
-
-    if not tz:
-        tz = tzlocal()
-
-    date = datetime.fromtimestamp(timestamp, tz=tz)
-
-    return date
