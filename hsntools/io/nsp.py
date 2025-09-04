3105dffe1706a0c940a1de3e24138a3c0a7cde1e
Tom Donoghue <tdonoghue.research@gmail.com>
2025-09-04 19:49:50 +0100

    move blackrock check file to IO

--- hsntools/io/nsp.py
+++ hsntools/io/nsp.py
@@ -2,6 +2,7 @@
 
 from hsntools.io.utils import check_folder
 from hsntools.modutils.dependencies import safe_import, check_dependency
+from hsntools.timestamps.utils import compute_sample_length
 
 neo = safe_import('neo')
 
@@ -34,3 +35,32 @@ def load_blackrock(file_name, folder, nsx_to_load=None, load_nev=None):
     reader.parse_header()
 
     return reader
+
+
+def check_blackrock_file_info(reader):
+    """Check some basic information and metadata from a set of Blackrock files.
+
+    Parameters
+    ----------
+    reader : neo.rawio.blackrockrawio.BlackrockRawIO
+        File reader for the Blackrock file(s).
+    """
+
+    str_fmt = '  seg#{}    start: {:1.2e}    stop: {:1.2e}    size: {:10d}    tlen: {:4.2f}'
+
+    fs = reader.get_signal_sampling_rate()
+    n_chans = reader.signal_channels_count(0)
+
+    print('sampling rate: \t', fs)
+    print('# channels: \t', n_chans)
+
+    n_blocks = reader.block_count()
+    for bi in range(n_blocks):
+        print('block #{}:'.format(bi))
+        n_segments = reader.segment_count(bi)
+        for si in range(n_segments):
+            seg_start = reader.segment_t_start(bi, si)
+            seg_stop = reader.segment_t_stop(bi, si)
+            seg_size = reader.get_signal_size(bi, si)
+            seg_length = compute_sample_length(seg_size, fs)
+            print(str_fmt.format(si, seg_start, seg_stop, seg_size, seg_length))
--- hsntools/tests/io/test_nsp.py
+++ hsntools/tests/io/test_nsp.py
@@ -8,3 +8,7 @@ from hsntools.io.nsp import *
 def test_load_blackrock():
     # Note: need to add dummy neo object to check this - skipped for now
     pass
+
+def test_check_blackrock_file_info():
+    # Note: need to add dummy neo object to check this - skipped for now
+    pass
